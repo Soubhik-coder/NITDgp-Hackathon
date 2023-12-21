b8215f2c45e76ed194ad8545595862a3d4904fe0
Abir Roy <roy-abir05@users.noreply.github.com>
2023-12-21 20:06:57 +0530

    "Creating Repo Page"

--- frontend/src/Pages/Repo/Repo.jsx
+++ frontend/src/Pages/Repo/Repo.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import Navbar from '../../Components/NavBar/Navbar'
+import React from "react";
+import Navbar from "../../Components/NavBar/Navbar";
+import "./Repo.scss";
 
 const Repo = () => {
-  return (
-    <div>
-        <Navbar/>
-        <div className="repo-container">
-            <div className="repo-main">
-                <div className="repo-header">
-                    <h2>Repo</h2>
-                    <div className='public'>Public</div>
-                </div>
-                <div className="branch-dropdown">
-                    <div className="branch">
-                        <select name="" id="">
-                            <option value="main">main</option>
-                            <option value="master">master</option>
-                        </select>
+
+    return (
+        <div>
+            <Navbar />
+            <div className="repo-container">
+                <div className="repo-main">
+                    <div className="repo-header">
+                        <h2>DRepo</h2>
+                        <div className="access">Public</div>
                     </div>
-                    <div className="code">
-                        <button>Code</button>
+                    <div className="branch-dropdown">
+                        <div className="branch">
+                            <select className="options">
+                                <option value="main">main</option>
+                                <option value="master">master</option>
+                            </select>
+                        </div>
+                        <div className="code">
+                            <button className="code-btn">
+                                Code
+                            </button>
+                        </div>
                     </div>
-                    <div className="file-list">
+                    <div className="files-list">
                         <table>
                             <thead>
                                 <tr>
@@ -37,7 +42,6 @@ const Repo = () => {
                                     <td>2 days ago</td>
                                 </tr>
                             </tbody>
-
                         </table>
                     </div>
                     <div className="readme-container">
@@ -45,34 +49,50 @@ const Repo = () => {
                             <h3>README.md</h3>
                         </div>
                         <div className="readme-body">
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident nam nostrum eaque animi deserunt cumque eius nemo maiores aspernatur consequatur, optio fugiat unde vel tempore necessitatibus saepe officiis obcaecati quas quae magnam veniam odit!</p>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                                explicabo qui vero voluptatibus ipsum. Saepe nesciunt
+                                laudantium laboriosam eligendi, numquam iste, odio aut quasi,
+                                minima alias est velit sunt culpa!
+                            </p>
                         </div>
-                        <div className="add-commit">
-                            <h2>Add new commit</h2>
-                            <div className="file-upload-container">
-                                <div className="file-upload">
-                                    <div className="drag-drop" id='dropzone'>
-                                        <span>Drag and Drop your files</span>
-                                        <input type="file" id="filepicker" name='filelist' webkitdirectory="" directory="" />
-                                        <button>Upload Directory</button>
-                                    </div>
+                    </div>
+                    <div className="add-commit">
+                        <h2>Add new commit</h2>
+                        <div className="file-upload-container">
+                            <div className="file-upload">
+                                <div className="drag-drop-area">
+                                    <span>Drag and Drop your files</span>
+                                    <input
+                                        type="file"
+                                        id="filepicker"
+                                        name="fileList"
+                                        webkitdirectory=""
+                                        directory=""
+                                    //   style={{ display: 'none' }}
+
+                                    />
+                                    <button className="select-files"
+                                    >Select Directory</button>
                                 </div>
-                                <input type="text" name='' id='' placeholder='Commit Message' />
-                                <button>Submit</button>
+                            </div>
+                            <div className="commit-section">
+                                <input type="text" name="" id="commitMessage" placeholder="Commit Message" />
+                                <button id="submitBtn">Submit</button>
                             </div>
                         </div>
                     </div>
-                    <div className="side-bar">
-                        <div className="about">
-                            <h3>About</h3>
-                            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci quia odit nobis consequatur maiores eum blanditiis voluptas sint beatae numquam ad quos quam expedita facilis voluptatem vel fugiat molestiae qui repellendus, pariatur, perspiciatis quasi!</p>
-                        </div>
+                </div>
+                <div className="repo-sidebar">
+                    <div className="about">
+                        <h3>About</h3>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Adipisci, assumenda labore nihil, velit sed voluptatem molestias incidunt ut ea illo ipsum porro facilis magnam? Fugit deleniti debitis reprehenderit, fugiat ipsum provident officiis praesentium ipsam voluptatem corrupti.</p>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-  )
-}
 
-export default Repo
+    );
+};
+
+export default Repo;
